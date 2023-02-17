3f54c7fa854460a8d07cc783eeda2aebd1c072fc
ChrisByrn3 <chris.byrn3@gmail.com>
2023-02-17 12:40:47 +0000

    Associations declared

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const {User} = require('./user')
 const {Board} = require('./Board')
 const {Cheese} = require('./Cheese')
 
+//User one to many Board association
+User.hasMany(Board);
+Board.belongsTo(User);
+
+//Board and Cheese many to many assoc
+Cheese.belongsToMany(Board, {through: 'Cheese_Board'});
+Board.belongsToMany(Cheese, {through: 'Cheese_Board'});
+
 
 
 module.exports = {
